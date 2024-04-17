075b420fc43c1bbd1b2cfc1f6579f5d4899d647d
almajulioo <alma.122140057@student.itera.ac.id>
2024-04-17 16:25:10 +0700

    add background

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import pygame
-
-#initialize the pygame 
+from pygame.locals import *
+# Inisialisasi Pygame
 pygame.init()
 
-screen = pygame.display.set_mode((800, 600))
+# Membuat dan mengatur ukuran windows
+screen = pygame.display.set_mode((800, 600), RESIZABLE)
+
+# Judul
+pygame.display.set_caption("Tower Defense")
+
+# Load background
+background = pygame.image.load("./Assets/img/grass_bg.jpg")
+screen.blit(pygame.transform.scale(background, (800, 600)), (0,0))
 
 running = True
 while running:
      for event in pygame.event.get():
           if event.type == pygame.QUIT:
                running = False
+          elif event.type == VIDEORESIZE:
+               screen = pygame.display.set_mode(event.dict['size'], RESIZABLE)
+               screen.blit(pygame.transform.scale(background, event.dict['size']), (0,0))
+               pygame.display.flip()
+
+     pygame.display.update()
