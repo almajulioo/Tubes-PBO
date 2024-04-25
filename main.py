127646ccc53765c52317e09c5b66bc482e36a13d
almajulioo <alma.122140057@student.itera.ac.id>
2024-04-25 21:48:54 +0700

    change to maze game

--- main.py
+++ main.py
@@ -4,23 +4,16 @@ from pygame.locals import *
 pygame.init()
 
 # Membuat dan mengatur ukuran windows
-screen = pygame.display.set_mode((800, 600), RESIZABLE)
+screen = pygame.display.set_mode((800, 600))
 
 # Judul
-pygame.display.set_caption("Tower Defense")
-
-# Load background
-background = pygame.image.load("./Assets/img/grass_bg.jpg")
-screen.blit(pygame.transform.scale(background, (800, 600)), (0,0))
+pygame.display.set_caption("Maze Game")
 
 running = True
 while running:
      for event in pygame.event.get():
           if event.type == pygame.QUIT:
                running = False
-          elif event.type == VIDEORESIZE:
-               screen = pygame.display.set_mode(event.dict['size'], RESIZABLE)
-               screen.blit(pygame.transform.scale(background, event.dict['size']), (0,0))
-               pygame.display.flip()
+         
 
      pygame.display.update()
