ee35409861ca746df93666fd97d9cada47221216
almajulioo <alma.122140057@student.itera.ac.id>
2024-04-29 03:35:05 +0700

    buat dinding yang lebih bagus

--- Object/Dinding.py
+++ Object/Dinding.py
@@ -3,14 +3,22 @@ import pygame
 semua_dinding = pygame.sprite.Group()
 
 class Dinding(pygame.sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self, x, y, sisi):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("./Assets/Img/Dinding/Dinding.png")
-
-        self.rect = self.image.convert_alpha().get_rect()
+        if sisi == "atas":
+            self.dinding = pygame.image.load("./Assets/Img/Dinding/dinding_atas.png")
+        elif sisi == "bawah":
+            self.dinding = pygame.image.load("./Assets/Img/Dinding/dinding_bawah.png")
+        elif sisi == "bawah2":
+            self.dinding = pygame.image.load("./Assets/Img/Dinding/dinding_bawah2.png")
+        elif sisi == "kanan" or sisi == "kiri":
+            self.dinding = pygame.image.load("./Assets/Img/Dinding/dinding_kiri.png")
+        elif sisi == "kanan2" or sisi == "kiri2":
+            self.dinding = pygame.image.load("./Assets/Img/Dinding/dinding_kiri2.png")
+        self.rect = self.dinding.convert_alpha().get_rect()
         self.rect.center = (x, y)
         semua_dinding.add(self)
 
     def update(self, screen):
-        screen.blit(self.image, self.rect)
+        screen.blit(self.dinding, self.rect)
         # pygame.draw.rect(screen, "Red", self.rect)
--- Object/Maps.py
+++ Object/Maps.py
@@ -4,36 +4,63 @@ from .Peti import Peti
 
 class Maps:
     MAPS = [
-        " DDDDDDDDDDDDDDDDDDDDDDDDD",
-        "                         D",
-        "D                  P     D",
-        "D                        D",
-        "D                        D",
-        "D                        D",
-        "D                        D",
-        "D                        D",
-        "D                        D",
-        "D                        D",
-        "D                        D",
-        "D                        D",
-        "D                        D",
-        "D                        D",
-        "D                        D",
-        "D                        D",
-        "D                        D",
-        "D                        D",
-        "DDDDDDDDDDDDDDDDDDDDDDDDDD",
+        "LAAAAAAAAAAAAAAAAAAAAAAAAL",
+        "                         K",
+        "K                  P     K",
+        "K                        K",
+        "L                        K",
+        "K                        K",
+        "K                        K",
+        "L                        K",
+        "K                        K",
+        "K                        K",
+        "L                        K",
+        "K                        K",
+        "K                        K",
+        "L                        K",
+        "K                        K",
+        "K                        K",
+        "K                        K",
+        "L                        K",
+        "LCBBCBBCBBBBBBBBBBCBBCBBCL",
     ]
 
     def __init__(self):
         x = 30
         y = 30
+        countrow = 0
+        countcol = 0
         for row in self.MAPS:
+            countrow+=1
+            print(countrow)
             for col in row:
-                if col == "D":
-                    Dinding(x, y)
-                if col == "P":
+                countcol += 1
+                if countrow == 1 and col == "L" and countcol == 1:
+                    x-= 9
+                    Dinding(x + 9, y, "kiri2")
+                elif countrow == 19 and col == "L" and countcol == 1:
+                    x-= 9
+                    Dinding(x + 9, y, "kiri2")
+                
+                elif col == "A":
+                    Dinding(x, y, "atas")
+                elif col == "K":
+                    if countcol == 26:
+                        Dinding(x - 18, y, "kiri")
+                    else:
+                        Dinding(x, y, "kiri")
+                elif col == "L":
+                    if countcol == 26:
+                        Dinding(x - 9, y, "kiri2")
+                    else:
+                        Dinding(x, y, "kiri2")
+                elif col == "B":
+                    Dinding(x, y + 8, "bawah")
+                elif col == "C":
+                    Dinding(x, y + 8, "bawah2")
+                elif col == "P":
                     Peti(x, y)
                 x += 30
+            countcol= 0
             y += 30
             x = 30
